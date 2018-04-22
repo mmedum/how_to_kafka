efe9fbd4c1b754ee41dc821a0e9ae237227828a4
Mark Medum Bundgaard <mmedum@gmail.com>
2018-04-22 23:03:54 +0200

    Adding consumer with polling for kafka

--- src/main/java/com/mmedum/howtokafka/consumer/MessageConsumer.java
+++ src/main/java/com/mmedum/howtokafka/consumer/MessageConsumer.java
@@ -1,4 +1,57 @@
 package com.mmedum.howtokafka.consumer;
 
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Collections;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MessageConsumer {
+
+    private static final Logger LOGGER = Logger.getLogger(MessageConsumer.class.getName());
+
+    private final static String TOPIC = "test-topic";
+    private final static String SERVERS = "localhost:9092";
+
+
+    public static void main(String[] args) {
+        LOGGER.setLevel(Level.INFO);
+        LOGGER.info("Starting up");
+        MessageConsumer consumer = new MessageConsumer();
+        consumer.runMessageConsumer();
+    }
+
+    private Consumer<Long, String> createConsumer() {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, SERVERS);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "MessageConsumer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        final Consumer<Long, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList(TOPIC));
+        return consumer;
+    }
+
+    private void runMessageConsumer(){
+        final Consumer<Long, String> consumer = createConsumer();
+        int noRecordsCount = 0;
+
+        while (true) {
+            final ConsumerRecords<Long, String> consumerRecords =
+                    consumer.poll(1000);
+
+            consumerRecords.forEach(record -> {
+                LOGGER.info("Consumer Record: (" + record.key() + ", " + record.value() +
+                                ", " + record.partition() + ", " +record.offset() + ")");
+            });
+
+            consumer.commitAsync();
+        }
+    }
 }
--- src/main/java/com/mmedum/howtokafka/producer/MessageProducer.java
+++ src/main/java/com/mmedum/howtokafka/producer/MessageProducer.java
@@ -8,6 +8,7 @@ import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class MessageProducer {
@@ -18,18 +19,23 @@ public class MessageProducer {
     private final static String TOPIC = "test-topic";
     private final static String SERVERS = "localhost:9092";
 
+    public static void main(String[] args){
+        LOGGER.setLevel(Level.INFO);
+        LOGGER.info("Starting up");
+        MessageProducer producer = new MessageProducer();
+        producer.runMessageProducer(10);
+    }
+
     private Producer<Long, String> createProducer(){
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, SERVERS);
         props.put(ProducerConfig.CLIENT_ID_CONFIG, "MessageProducer");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                LongSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                StringSerializer.class.getName());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         return new KafkaProducer<>(props);
     }
 
-    public void runMessageProducer(final int numberOfMessagesToSend){
+    private void runMessageProducer(final int numberOfMessagesToSend){
         final Producer<Long, String> producer = createProducer();
         for(long i=0; i<numberOfMessagesToSend; i++){
 
